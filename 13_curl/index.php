567dd84b34482e61f56e76f83fa7c8788d51e648
yanliu1111 <liuyan0504@gmail.com>
2023-06-19 16:55:38 -0600

    feat: class 13 done

--- 13_curl/index.php
+++ 13_curl/index.php
@@ -1,10 +1,38 @@
 <?php
 
 $url = 'https://jsonplaceholder.typicode.com/users';
-// Sample example to get data.
+// // Sample example to get data.
+// $resource = curl_init($url);
+// curl_setopt($resource, CURLOPT_RETURNTRANSFER, true);
+// $result = curl_exec($resource);
 
-// Get response status code
+// curl_close($resource);
 
-// set_opt_array
+// // Get response status code
+// $code = curl_getinfo($resource, CURLINFO_HTTP_CODE);
 
-// Post request
+// echo '<pre>';
+// var_dump($code);
+// echo '</pre>';
+// echo $result;
+
+$resource = curl_init();
+
+$user = [
+    'name' => 'John Doe',
+    'username' => 'john',
+    'email' => 'john@example.com'
+];
+
+curl_setopt_array($resource, [
+    CURLOPT_URL => $url,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => json_encode($user),
+    CURLOPT_HTTPHEADER => array('Content-Type: application/json')
+]);
+$result = curl_exec($resource);
+curl_close($resource);
+echo '<pre>';
+var_dump ($result);
+echo '</pre>';
