56e10dcc182cdbfb99e5f10826ac2579daa8c99c
yanliu1111 <liuyan0504@gmail.com>
2023-06-19 22:51:12 -0600

    styles: extension for php format

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -11,44 +11,48 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC); // Fetch all products
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Product CRUD</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="app.css">
-  </head>
-  <body>
+</head>
+
+<body>
     <h1>Product CRUD</h1>
     <p>
         <a href="create.php" class="btn btn-success">Create</a>
     </p>
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Image</th>
-      <th scope="col">Title</th>
-      <th scope="col">Price</th>
-      <th scope="col">Create Date</th>
-      <th scope="col">Action</th>
-    </tr>
-  </thead>
-  <tbody>  
-    <?php foreach ($products as $i => $product): ?>
-        <tr>
-            <th scope="row"><?php echo $i+1 ?></th>
-            <td></td>
-            <td><?php echo $product['title'] ?></td>
-            <td><?php echo $product['price'] ?></td>
-            <td><?php echo $product['create_date'] ?></td>
-            <td>
-            <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
-            <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
-            </td>
-        </tr>
-    <?php endforeach; ?>
-   </tbody>
-</table>
-  </body>
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Image</th>
+                <th scope="col">Title</th>
+                <th scope="col">Price</th>
+                <th scope="col">Create Date</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($products as $i => $product): ?>
+            <tr>
+                <th scope="row"><?php echo $i+1 ?></th>
+                <td></td>
+                <td><?php echo $product['title'] ?></td>
+                <td><?php echo $product['price'] ?></td>
+                <td><?php echo $product['create_date'] ?></td>
+                <td>
+                    <button type="button" class="btn btn-sm btn-outline-primary">Edit</button>
+                    <button type="button" class="btn btn-sm btn-outline-danger">Delete</button>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+
 </html>
