c6fde1bb0adf80d769c26628a1da1aa642f15be1
yanliu1111 <liuyan0504@gmail.com>
2023-06-19 22:51:28 -0600

    feat: GET and POST done

--- 14_product_crud/create.php
+++ 14_product_crud/create.php
@@ -1,40 +1,48 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Error connection threw exception
+// super global variable $_POST
+echo '<pre>';
+var_dump($_POST);
+echo '</pre>';
 
 ?>
 
 <!doctype html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Product CRUD</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="app.css">
-  </head>
-  <body>
+</head>
+
+<body>
     <h1>Create New Product</h1>
-    <form>
-  <div class="mb-3">
-    <label class="form-label">Product Image</label>
-    <br>
-    <input type="file">
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Product Title</label>
-    <input type="text" class="form-control" >
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Product Description</label>
-    <textarea class="form-control" ></textarea>
-  </div>
-  <div class="mb-3">
-    <label class="form-label">Product Price</label>
-    <input type="number" step=".01" class="form-control" >
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    
-  </body>
+    <form action="" method="post">
+        <div class="mb-3">
+            <label class="form-label">Product Image</label>
+            <br>
+            <input type="file" name="image">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Product Title</label>
+            <input type="text" name="title" class="form-control">
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Product Description</label>
+            <textarea name="description" class="form-control"></textarea>
+        </div>
+        <div class="mb-3">
+            <label class="form-label">Product Price</label>
+            <input type="number" step=".01" name="price" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+</body>
+
 </html>
